e57abcfcd13fdb8bb553b455350fc3dd9cf5adf3
Stef <xkristianwilliamsx@gmail.com>
2025-02-07 09:45:12 +0200

    test

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,10 @@
 console.log('pixabay');
+
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = SimpleLightBox('.gallery a', {
+  // options
+});
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,1 +1,6 @@
 console.log(render);
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
